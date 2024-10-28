b446b1cd252d84ba8ecf5a2e4e63591cec665747
AIE\s221250 <s221250@students.aie.edu.au>
2024-10-28 15:03:02 +1100

    Finalised merge

--- Project ChronoSpeed 2.0/Assets/Scripts/PropHit.cs
+++ Project ChronoSpeed 2.0/Assets/Scripts/PropHit.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PropHit : MonoBehaviour
@@ -8,7 +6,7 @@ public class PropHit : MonoBehaviour
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponentInParent<Rigidbody>();
     }
 
     private void OnTriggerEnter(Collider other)
