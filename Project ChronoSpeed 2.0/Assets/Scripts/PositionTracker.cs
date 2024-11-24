21e7974dff970e16c0b07a20e2aad25e7669004e
C:\Users\JamsW\Documents\SourceTree <JamsWWstar1091@gmail.com>
2024-11-24 21:07:32 +1100

    Map updated to work with different icons

--- Project ChronoSpeed 2.0/Assets/Scripts/PositionTracker.cs
+++ Project ChronoSpeed 2.0/Assets/Scripts/PositionTracker.cs
@@ -8,9 +8,12 @@ public class PositionTracker : NetworkBehaviour
 {
     [SerializeField] Transform minimapParent;
     [SerializeField] Image playerIconPrefab;
+    [SerializeField] Image otherPlayerIconPrefab;
+    [SerializeField] Image aiIconPrefab;
     [SerializeField] Vector3 offset;
     Collider boundsToUVCol;
     Vector3 extents;
+    Vector2 minimapSizeDelta;
 
     Vector3 maxes;
     Vector3 mins;
@@ -38,6 +41,8 @@ public class PositionTracker : NetworkBehaviour
         mins = transform.position - extents;
         xRange = maxes.x - mins.x;
         zRange = maxes.z - mins.z;
+
+        minimapSizeDelta = minimapParent.GetComponent<RectTransform>().sizeDelta;
     }
 
     void AddPlayer(Transform plyr, Image plyrIcon)
@@ -45,30 +50,43 @@ public class PositionTracker : NetworkBehaviour
         players.Add(new PlayerMapInfo(plyr, plyrIcon));
     }
 
+    void RemovePlayer(Transform plyr)
+    {
+        for(int i = 0; i < players.Count; i++)
+        {
+            if (players[i].PlayerTrans == plyr)
+            {
+                Destroy(players[i].PlayerIcon);
+                players.Remove(players[i]);
+                break;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         var fkCol = other.GetComponent<FakeCollision>();
         if(fkCol != null)
         {
-            AddPlayer(fkCol.myTransform, Instantiate(playerIconPrefab, minimapParent));
+            if(fkCol.myTransform.tag == "AI")
+            { AddPlayer(fkCol.myTransform, Instantiate(aiIconPrefab, minimapParent)); }
+            else if(fkCol.myTransform.tag == "OtherPlayer")
+            { AddPlayer(fkCol.myTransform, Instantiate(otherPlayerIconPrefab, minimapParent)); }
+            else if(fkCol.myTransform.tag == "Player")
+            { AddPlayer(fkCol.myTransform, Instantiate(playerIconPrefab, minimapParent)); }
         }
     }
-    private void OnTriggerStay(Collider other)
+
+    private void Update()
     {
-        var fkCol = other.GetComponent<FakeCollision>();
-        if(fkCol != null)
+        for(int i = 0; i < players.Count; i++)
         {
             Vector2 mappedVal;
-            mappedVal.x = (fkCol.myTransform.position.x - offset.x - mins.x) / xRange;
-            mappedVal.y = (fkCol.myTransform.position.z - offset.z - mins.z) / zRange;
-            for(int i = 0; i < players.Count; i++)
-            {
-                if (players[i].PlayerTrans == fkCol.myTransform)
-                {
-                    var rectTrans = players[i].PlayerIcon.transform.GetComponent<RectTransform>();
-                    rectTrans.anchoredPosition = mappedVal * rectTrans.parent.GetComponent<RectTransform>().sizeDelta;
-                }
-            }
+            mappedVal.x = (players[i].PlayerTrans.position.x - offset.x - mins.x) / xRange;
+            mappedVal.y = (players[i].PlayerTrans.position.z - offset.z - mins.z) / zRange;
+
+            players[i].PlayerIcon.rectTransform.anchoredPosition = 
+                mappedVal * minimapSizeDelta;
         }
+        
     }
 }
