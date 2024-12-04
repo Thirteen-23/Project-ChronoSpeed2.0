d92b78d9492955717e8de8dc629208b2f6f6fe0a
Gabriel <gabriel.ubay@gmail.com>
2024-12-04 12:30:01 +1100

    lowered rpm but increased power for Heavy car

--- Project ChronoSpeed 2.0/Assets/Boxtrigger.cs
+++ Project ChronoSpeed 2.0/Assets/Boxtrigger.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class Boxtrigger : MonoBehaviour
 {
     [SerializeField] Image[] arrowSignForDirection;
-    [SerializeField] int arrowIndex; 
-
+    [SerializeField] int arrowIndex;
+    [SerializeField] float timer;
+    [SerializeField] float signDuration;
+    [SerializeField] float valueToLerp; 
     private void FixedUpdate()
     {
-        
 
+       
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.attachedRigidbody.tag == "Player")
         {
-            arrowSignForDirection[arrowIndex].color = new Color(255, 255, 255, 1); 
+            arrowSignForDirection[arrowIndex].color = new Color(255, 255, 255,Mathf.Lerp(0,1, 5)); 
         }
     }
     private void OnTriggerExit(Collider other)
